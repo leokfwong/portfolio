fb31ef3465c246fd94cc0df7b72a4b67d0cf9b96
leokfwong <leo.wong2@mail.mcgill.ca>
2020-01-08 23:08:43 -0500

    Implement type erase

---
+++ assets/js/functions.js
@@ -0,0 +1,48 @@
+window.onload = function() {
+	console.log("Javascript working, website loaded.");
+	// Type Erase
+	const passions_data = ["Data Science.   ", "Web Development.   ", "Cybersecurity.   "];
+	let i = 0;
+	let timer;
+	let type_interval = 150;
+	let erase_interval = 100;
+	// Find div containing text
+	let div = document.getElementById("passions-type-erase");
+	div.innerHTML = "";
+	// Typing effect
+	function typingEffect() {
+		let word = passions_data[i].split("");
+		var loopTyping = function() {
+			if (word.length > 0) {
+				div.innerHTML += word.shift();
+			} else {
+				deletingEffect();
+				return false;
+			};
+			timer = setTimeout(loopTyping, type_interval);
+		};
+		loopTyping();
+	};
+	// Deleting effect
+	function deletingEffect() {
+		let word = passions_data[i].split("");
+		var loopDeleting = function() {
+			if (word.length > 0) {
+				word.pop();
+				div.innerHTML = word.join("");
+			} else {
+				if (passions_data.length > (i + 1)) {
+					i++;
+				} else {
+					i = 0;
+				};
+				typingEffect();
+				return false;
+			};
+			timer = setTimeout(loopDeleting, erase_interval);
+		};
+		loopDeleting();
+	};
+	// Call typing effect function
+	typingEffect();
+}
