b43906592734a32a859b0b98a8881f4b84edf355
leokfwong <leo.wong2@mail.mcgill.ca>
2020-01-15 01:16:18 -0500

    Implement hamburger menu

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,9 +11,25 @@ window.onload = function() {
 	if (url.search("/blog/") > -1) {
 		on_homepage = false
 	};
+
+	// Hamburger on mobile
+	let hamburger_button = document.getElementById("nav-bar-hamburger");
+	hamburger_button.addEventListener("click", function() {
+		let mobile_menu = document.getElementById("nav-bar-menu");
+		if (mobile_menu.className == "nav-bar-menu-desktop") {
+			mobile_menu.className = "nav-bar-menu-mobile";	
+		} else {
+			mobile_menu.className = "nav-bar-menu-desktop";
+		}
+	});
+
 	if (on_homepage) {
 		if (view == "blog") {
 			displayBlog();
+		} else if (view == "projects") {
+			displayProject();
+		} else {
+			console.log("About me.")
 		}
 		// Type Erase
 		const passions_data = ["Data Science.   ", "Web Development.   ", "Cybersecurity.   "];
@@ -66,21 +82,12 @@ window.onload = function() {
 		let proj_data = document.getElementById("projects-menu-data");
 		let proj_web = document.getElementById("projects-menu-web");
 		proj_featured.addEventListener("click", function() {
-			proj_featured.className = "projects-menu-item projects-menu-active";
-			proj_data.className = "projects-menu-item";
-			proj_web.className = "projects-menu-item";
 			displayProjects("featured");
 		});
 		proj_data.addEventListener("click", function() {
-			proj_featured.className = "projects-menu-item";
-			proj_data.className = "projects-menu-item projects-menu-active";
-			proj_web.className = "projects-menu-item";
 			displayProjects("data");
 		});
 		proj_web.addEventListener("click", function() {
-			proj_featured.className = "projects-menu-item";
-			proj_data.className = "projects-menu-item";
-			proj_web.className = "projects-menu-item projects-menu-active";
 			displayProjects("web");
 		});
 
@@ -89,18 +96,32 @@ window.onload = function() {
 
 	// Alternate between about and blog
 	let about_button = document.getElementById("nav-bar-about");
+	let projects_button = document.getElementById("nav-bar-projects");
 	let blog_button = document.getElementById("nav-bar-blog");
 
 	about_button.addEventListener("click", function() {
 		if (on_homepage) {
-			document.getElementById("side-page").style.display = "none";
+			document.getElementById("side-page-blog").style.display = "none";
+			document.getElementById("nav-bar-projects").className = "nav-bar-item";
 			document.getElementById("nav-bar-blog").className = "nav-bar-item";
-			document.getElementById("main-page").style.display = "block";
+			document.getElementById("about-me-section").style.display = "block";
+			document.getElementById("experience-section").style.display = "block";
+			document.getElementById("projects-section").style.display = "block";
+			document.getElementById("publications-section").style.display = "block";
 			document.getElementById("nav-bar-about").className = "nav-bar-item nav-bar-active";
 			window.scrollTo(0, 0);
 			localStorage.setItem("view", "about");
 		} else {
 			localStorage.setItem("view", "about");
+			window.location.href = "../../index.html";
+		}
+	});
+	projects_button.addEventListener("click", function() {
+		if (on_homepage) {
+			displayProject();
+			localStorage.setItem("view", "projects");
+		} else {
+			localStorage.setItem("view", "projects");
 			window.location.href = "../../index.html";
 		}
 	});
@@ -128,6 +149,21 @@ window.onload = function() {
 }
 
 function displayProjects(category) {
+	let proj_featured = document.getElementById("projects-menu-featured");
+	let proj_data = document.getElementById("projects-menu-data");
+	let proj_web = document.getElementById("projects-menu-web");
+	proj_featured.className = "projects-menu-item";
+	proj_data.className = "projects-menu-item";
+	proj_web.className = "projects-menu-item";
+	if (category == "featured") {
+		proj_featured.className += " projects-menu-active";
+	} else if (category == "data") {
+		proj_data.className += " projects-menu-active";
+	} else if (category == "web") {
+		proj_web.className += " projects-menu-active";
+	} else {
+		// Other category
+	}
 	let project_container = document.getElementById("projects-content");
 	let projects = project_container.childNodes;
 	for (let i = 0; i < projects.length; i++) {
@@ -142,15 +178,39 @@ function displayProjects(category) {
 	}
 }
 
-function displayBlog() {
-	document.getElementById("main-page").style.display = "none";
-	document.getElementById("nav-bar-blog").className = "nav-bar-item nav-bar-active";
-	document.getElementById("side-page").style.display = "block";
+function displayProject() {
+	displayProjects("featured");
+	document.getElementById("about-me-section").style.display = "none";
+	document.getElementById("experience-section").style.display = "none";
+	document.getElementById("projects-section").style.display = "block";
+	document.getElementById("publications-section").style.display = "none";
+	document.getElementById("side-page-blog").style.display = "none";
+	document.getElementById("nav-bar-projects").className = "nav-bar-item nav-bar-active";
+	document.getElementById("nav-bar-blog").className = "nav-bar-item";
 	document.getElementById("nav-bar-about").className = "nav-bar-item";
 
 	viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 	if (viewportWidth < 1024) {
-		document.getElementById("side-page").scrollIntoView();
+		document.getElementById("projects-section").scrollIntoView();
+		window.scrollBy(0, -70);
+	} else {
+		window.scrollTo(0, 0);
+	}
+}
+
+function displayBlog() {
+	document.getElementById("about-me-section").style.display = "none";
+	document.getElementById("experience-section").style.display = "none";
+	document.getElementById("projects-section").style.display = "none";
+	document.getElementById("publications-section").style.display = "none";
+	document.getElementById("nav-bar-blog").className = "nav-bar-item nav-bar-active";
+	document.getElementById("side-page-blog").style.display = "block";
+	document.getElementById("nav-bar-about").className = "nav-bar-item";
+	document.getElementById("nav-bar-projects").className = "nav-bar-item";
+
+	viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+	if (viewportWidth < 1024) {
+		document.getElementById("side-page-blog").scrollIntoView();
 		window.scrollBy(0, -70);
 	} else {
 		window.scrollTo(0, 0);
