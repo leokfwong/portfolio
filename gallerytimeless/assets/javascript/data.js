656293d00a2e14de2387edfc2371c83b0bc514e9
Leo Wong <leo.wong2@mail.mcgill.ca>
2021-02-24 00:15:27 -0500

    Add new font and reposition mobile layout

--- gallerytimeless/assets/javascript/data.js
+++ gallerytimeless/assets/javascript/data.js
@@ -3,357 +3,357 @@ const gallery_json = [{
     "year": "2008",
     "title": "Apples",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil Painting",
+    "type": "oil Painting",
     "orientation": "landscape"
 }, {
     "id": "002",
     "year": "2008",
     "title": "Salad Ingredients Still Life",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "003",
     "year": "2008",
     "title": "Fruit Basket Still Life",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "004",
     "year": "2008",
     "title": "Vegetables Still Life",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "005",
     "year": "2008",
     "title": "Flowers Still Life",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "006",
     "year": "2008",
     "title": "Serenity",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "007",
     "year": "2009",
     "title": "Walk in the Park",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "008",
     "year": "2009",
     "title": "Countryside Snow",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "009",
     "year": "2009",
     "title": "Old Port, Montreal",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "010",
     "year": "2009",
     "title": "Dock",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "011",
     "year": "2009",
     "title": "Night View of Bridge",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "012",
     "year": "2009",
     "title": "Portrait of Man",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "013",
     "year": "2010",
     "title": "Venice",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "014",
     "year": "2010",
     "title": "The Colosseum",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "015",
     "year": "2010",
     "title": "Winter Snow",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "016",
     "year": "2010",
     "title": "Lady Agnew of Lochnaw - John Singer Sargent",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "017",
     "year": "2011",
     "title": "Shade",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "018",
     "year": "2011",
     "title": "Golden Leaves",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "019",
     "year": "2011",
     "title": "Wisdom",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "020",
     "year": "2011",
     "title": "Luminescence",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "021",
     "year": "2011",
     "title": "Woman with a Parasol, Claude Monet",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "022",
     "year": "2011",
     "title": "Portrait of Man",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "023",
     "year": "2012",
     "title": "Flowers Still Life",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "024",
     "year": "2012",
     "title": "Chinatown, Montreal",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "025",
     "year": "2012",
     "title": "Architecture",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "026",
     "year": "2012",
     "title": "Portrait of Man",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "027",
     "year": "2012",
     "title": "Pond and Water Lilies",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "028",
     "year": "2013",
     "title": "Midnight Waves",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "029",
     "year": "2013",
     "title": "Italian Cafe",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "030",
     "year": "2013",
     "title": "Tranquility",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "031",
     "year": "2013",
     "title": "Winter evening in Chinatown, Montreal",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "032",
     "year": "2013",
     "title": "Creek",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "033",
     "year": "2013",
     "title": "Venice Gondolas",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "034",
     "year": "2014",
     "title": "Running in Circles",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "035",
     "year": "2014",
     "title": "Gathering",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "036",
     "year": "2014",
     "title": "Winter Evening",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "037",
     "year": "2014",
     "title": "Afternoon Tea",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "038",
     "year": "2014",
     "title": "Street Perspective",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "039",
     "year": "2014",
     "title": "Watermelon Girl",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "040",
     "year": "2015",
     "title": "Waves",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "041",
     "year": "2015",
     "title": "Portrait",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "042",
     "year": "2015",
     "title": "Shanghai Skycrapers",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "043",
     "year": "2015",
     "title": "Crowd",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "044",
     "year": "2015",
     "title": "Haze",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "045",
     "year": "2015",
     "title": "Autumn in Montreal",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "046",
     "year": "2015",
     "title": "The Parc Monceau, Claude Monet",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "047",
     "year": "2016",
     "title": "Self-portrait of Rembrandt",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "048",
     "year": "2016",
     "title": "Old Montreal",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "049",
     "year": "2016",
     "title": "Streets of Montreal",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "050",
     "year": "2016",
     "title": "Saint Lawrence River",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "051",
     "year": "2016",
     "title": "Autumn Colors",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "052",
@@ -367,125 +367,125 @@ const gallery_json = [{
     "year": "2017",
     "title": "Rainy Day in New York",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "054",
     "year": "2017",
     "title": "Girl Portrait",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "055",
     "year": "2017",
     "title": "Reflection",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "056",
     "year": "2017",
     "title": "Swamp",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "057",
     "year": "2017",
     "title": "Old Champlain Bridge",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "058",
     "year": "2018",
     "title": "McGill Statue",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "059",
     "year": "2018",
     "title": "Sunset by the Shore",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "060",
     "year": "2018",
     "title": "Under the Bridge",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "061",
     "year": "2018",
     "title": "Self-portrait of Myself",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "062",
     "year": "2018",
     "title": "Self Portrait with a Beret",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "063",
     "year": "2018",
     "title": "Percé Rock",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "064",
     "year": "2019",
     "title": "East Coast Sunset",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "065",
     "year": "2019",
     "title": "Niagara Falls",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "landscape"
 }, {
     "id": "066",
     "year": "2019",
-    "title": "Cheung Chau at Night 1",
+    "title": "Night in Cheung Chau",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "067",
     "year": "2019",
     "title": "Winter St-Denis Steet",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "068",
     "year": "2019",
     "title": "Ferris Wheel Old Port",
     "size": "6\" x 6\" (15.2cm x 15.2cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "069",
     "year": "2019",
     "title": "Billy Graham Portrait",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }, {
     "id": "070",
     "year": "2020",
-    "title": "Cheung Chau at Night 2",
+    "title": "Night in Cheung Chau",
     "size": "16\" x 20\" (40cm x 50cm)",
-    "type": "Oil painting",
+    "type": "oil painting",
     "orientation": "portrait"
 }]
